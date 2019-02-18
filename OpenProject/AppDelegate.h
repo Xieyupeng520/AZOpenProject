a3233d593a7b4d5ea7ddf0b3a9b22725b314d9fe
AZZ <806622085@qq.com>
2019-02-18 21:47:17 +0800

    Initial Commit

---
+++ OpenProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenProject
+//
+//  Created by 阿曌 on 2019/2/18.
+//  Copyright © 2019 阿曌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenProject
+//
+//  Created by 阿曌 on 2019/2/18.
+//  Copyright © 2019 阿曌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
